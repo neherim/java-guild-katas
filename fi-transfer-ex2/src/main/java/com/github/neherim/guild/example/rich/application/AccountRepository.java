4efd32810359d7e8443ff88c6a6c181f09afac83
Artem Mikhailovsky <artem.mikhailovskyi@gmail.com>
2022-06-10 00:59:18 +0300

    updated

--- fi-transfer-ex2/src/main/java/com/github/neherim/guild/example/rich/application/AccountRepository.java
+++ fi-transfer-ex2/src/main/java/com/github/neherim/guild/example/rich/application/AccountRepository.java
@@ -3,16 +3,20 @@ package com.github.neherim.guild.example.rich.application;
 import com.github.neherim.guild.example.rich.domain.Account;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Component
-public interface AccountRepository extends CrudRepository<Account, Long> {
+public interface AccountRepository extends Repository<Account, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select a from Account a where a.id = :id")
     Optional<Account> findByIdWithLock(Long id);
+
+    Account save(Account entity);
+
+    Optional<Account> findById(Long id);
 }
